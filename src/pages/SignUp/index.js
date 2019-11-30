6ad6ef35130ce4bfa34448d94593f00934244a7f
Alisson Henrique <alisson-henri@live.com>
2019-11-30 18:00:57 -0300

    finish rotye initial

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,8 @@
-import React, {useRef, useState} from 'react';
-import {Image} from 'react-native';
-import {useDispatch, useSelector} from 'react-redux';
+import React, { useRef, useState } from 'react';
+import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 import logo from '~/assets/logo.png';
-import {signUpRequest} from '~/store/modules/auth/actions';
+import { signUpRequest } from '~/store/modules/auth/actions';
 import Background from '~/components/Background';
 import {
   Container,
@@ -13,8 +13,9 @@ import {
   SingLinkText,
 } from './styles';
 
-const loading = useSelector(state => state.auth.loading);
-export default function SignUp({navigation}) {
+
+export default function SignUp({ navigation }) {
+  const loading = useSelector(state => state.auth.loading);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
